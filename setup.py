815059bc3dde323a6c91722491f7da846f1cab50
Alem Memic <alem.memic@dynamicasoft.io>
2021-05-20 07:37:31 +0200

    cleaning the code

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
+import pathlib
 import setuptools
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.0'
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+
+VERSION = '0.1.1'
 
 setuptools.setup(
     name="sympan",
     version=VERSION,
     author="Alem Memic",
     author_email="memicalem@gmail.com",
-    description="",
+    description="Python library for downloading anything from AWS S3",
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_packages(),
-
-    zip_safe=False,
+    license="MIT",
+    url="https://github.com/alem88/sympan",
+    packages=["sympan"],
     include_package_data=True,
     classifiers=[
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "License :: I hope it is going to be useful",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
     ],
     install_requires=[
         "boto3==1.15.18",
