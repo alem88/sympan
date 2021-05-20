ab544e8962c61720e92bcc1dac825688fe496267
Alem Memic <alem.memic@dynamicasoft.io>
2021-05-20 07:13:40 +0200

    small change

--- demo/demo_download_locally.py
+++ demo/demo_download_locally.py
@@ -4,16 +4,11 @@ from credentials import (AWS_ACCESS_KEY_ID,
                          AWS_SECRET_ACCESS_KEY,
                          REGION_NAME)
 
-from config import (
-    S3_URL,
-    DESTINATION_FOLDER_PATH
-)
-
 if __name__ == '__main__':
     s3 = S3Handler(aws_access_key_id=AWS_ACCESS_KEY_ID,
                    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                    region_name=REGION_NAME)
 
-    s3.download_locally(s3_url= S3_URL,
-                        destination_folder=DESTINATION_FOLDER_PATH,
+    s3.download_locally(s3_url= "s3://url/path/to/folder/or/file/",
+                        destination_folder="local/path/to/destination/folder",
                         n_jobs=2)
